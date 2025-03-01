8ae457419d4666c4ae598db3405494b748db3534
nephyt <bey_je@hotmail.com>
2025-03-01 09:16:03 -0500

    chore: change url for dev

--- src/main/java/com/pingpong/ui/Constants.java
+++ src/main/java/com/pingpong/ui/Constants.java
@@ -2,9 +2,9 @@ package com.pingpong.ui;
 
 public class Constants {
 
-    public static final String SERVICE_PLAYER_URL = "http://localhost:8090/";
-    public static final String SERVICE_GAME_URL = "http://localhost:8070/";
-   public static final String SERVICE_COUNT_URL = "http://localhost:8060/";
+    public static final String SERVICE_PLAYER_URL = "http://172.17.0.5:8090/";
+    public static final String SERVICE_GAME_URL = "http://172.17.0.4:8070/";
+   public static final String SERVICE_COUNT_URL = "http://172.17.0.3:8060/";
 
     //public static final String SERVICE_PLAYER_URL = "https://pingpongplayersservice.herokuapp.com/";
     //public static final String SERVICE_GAME_URL = "https://pingponggamesservice.herokuapp.com/";
